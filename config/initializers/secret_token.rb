5ac5a3a8d1665201d68a24cadea499503fd1f3b5
ilnar <your.email@example.com>
2014-02-16 09:56:30 +0400

    add devise and botstrsp and static_pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metastudio::Application.config.secret_token = 'a56a8e1eb2b500b66210fb1d62e4a9393972975b8a0502e2074fbf513ad49cf981eba529e2282baaa1637ddaf8826aa5fea91084d66578644380f15280a8219a'
