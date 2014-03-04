98f8219d2ec67854ffc2d75d445e73412a6d5b96
ilnar <your.email@example.com>
2014-03-04 18:45:23 +0400

    install Ajax

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,12 +7,13 @@ class MicropostsController < ApplicationController
   
   def create
     @micropost = current_user.microposts.build(params[:micropost])
-    if @micropost.save
-      flash[:success] = "Micropost created"
-      redirect_to root_path
-    else
-      @feed_items = []
-      render 'static_pages/home'  
+    respond_to do |format|
+      if @micropost.save
+        format.html { redirect_to root_path }
+        format.js
+      else
+        format.html { render 'static_pages/home' }
+      end  
     end
   end
 
@@ -40,6 +41,9 @@ class MicropostsController < ApplicationController
   def destroy
     @micropost = Micropost.find(params[:id])
     @micropost.destroy
-    redirect_to root_url
+    respond_to do |format|
+    format.html { redirect_to root_url }
+    format.js
   end
+ end
 end
